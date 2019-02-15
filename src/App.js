ad6fa04fbf57badb028287ecb76f5ca29ea92acd
Karolina Kadela <giemolla@MacBook-Pro-Kamil.local>
2019-02-15 13:33:59 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,70 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+const Book = (props) => {
+  return(
+    <div>
+      <h2>{props.title}</h2>
+    </div>
+  );
+};
+
+class LibraryApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dupa: 'dupa',
+      books: []
+    };
+  }
+
+  componentWillMount() {
+    this.fetchBooks();
+  }
+
+  fetchBooks() {
+    const url = 'http://localhost:3001/books';
+    fetch(url)
+      .then(resp => resp.json())
+      .then(data => {
+        this.setState({ books: data });
+        console.log(this.state.books);
+      });
+  }
+
+  // addBook() {
+  //   const url = 'http://localhost:3001/books';
+  //   const newBook = {
+  //     title: "Ojciec Chrzestny",
+  //     author: "Mario Puzo"
+  //   };
+  //   console.log('Dodaję...');
+  //   fetch(url, {
+  //     method: 'post',
+  //     headers: {
+  //       "Content-type": "application/json; charset=UTF-8"
+  //     },  
+  //     body: JSON.stringify(newBook)
+  //   })
+  //   .then(res => res.json())
+  //   .then(res => {
+  //       console.log("Dodana ksiązka: ");
+  //       console.log(res);
+  //   });
+  // }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+    return(
+      <div>
+        {this.state.dupa}
+        {this.state.books.length}
+        {this.state.books.map(book => {
+          return book.title;
+        })}
+        <Book title={this.state.books[0].title}/>
       </div>
     );
   }
 }
 
-export default App;
+export default LibraryApp;
